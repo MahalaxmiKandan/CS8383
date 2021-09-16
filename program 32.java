aa743279a18d772d4708bae602014a2b4997b66b
MahalaxmiKandan <89631410+MahalaxmiKandan@users.noreply.github.com>
2021-09-17 05:09:18 +0530

    program 32.java

---
+++ program 32.java
@@ -0,0 +1,8 @@
+	public class TestSubstring{  
+	 public static void main(String args[]){  
+	String s="  Sachin  ";  
+	System.out.println(s);//  Sachin    
+	System.out.println(s.trim());//Sachin  
+
+}
+}
